c3663e4184d091417e6808d4e0c793bca24293e6
Lhyo <leo_666_aries@hotmail.com>
2017-06-21 15:10:59 -0500

    Add files via upload

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+class LoginTest extends DuskTestCase
+{
+    //use DatabaseMigrations;
+    use DatabaseTransactions;
+
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    /**
+     * @group usuarios
+     */
+    public function testUsuarioIncorrecto()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->type('email', 'asdf@asdf.com')
+                    ->type('password', 'secret')
+                    ->press('Aceptar')
+                    ->assertSee('Usuario incorrecto.');
+        });   
+    }
+
+    /**
+     * @group usuarios
+     */
+    public function testPasswordIncorrecto()
+    {
+        
+        $user = factory(User::class)->create([
+            'password' => bcrypt('123456')
+        ]);
+
+       $this->browse(function (Browser $browser)use($user) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'secret')
+                    ->press('Aceptar')
+                    ->assertSee('Contraseña incorrecta.');
+        });
+
+       User::destroy($user->id);
+    }
+
+    /**
+     * @group usuarios
+     */
+    public function testUsuarioCorrecto()
+    {
+        
+        $user = factory(User::class)->create([
+            'password' => bcrypt('123456')
+        ]);
+
+       $this->browse(function (Browser $browser)use($user){
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', '123456')
+                    ->press('Aceptar')
+                    ->assertSee('Bienvenido');
+        });
+       User::destroy($user->id);
+    }
+}
